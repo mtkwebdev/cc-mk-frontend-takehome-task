dc214df803cad722f921ef7e05491b5f22badf9a
Mohammad Kawadri <62560333+mtkwebdev@users.noreply.github.com>
2024-10-08 22:23:48 +0100

    InputField: work in progress

--- src/components/common/InputFIeld/styles.js
+++ src/components/common/InputFIeld/styles.js
@@ -1,1 +1,15 @@
 import styled from "styled-components";
+
+export const InputFIeldContainer = styled.div`
+  display: flex;
+  border: 1px solid grey;
+  border-radius: 0.5rem;
+  padding: 0.2rem;
+`;
+export const InputFIeld = styled.input`
+  border: none;
+  flex: 1 0 max-content;
+  width: max-content;
+  padding: 0.9rem;
+  outline: none;
+`;
